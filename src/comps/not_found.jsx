434b481ec87378960b68d44fe7f7eb079bf3a94b
hi-matbub <6matbub@gmail.com>
2020-07-17 13:41:21 -0700

    404 & router

---
+++ src/comps/not_found.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const NotFound = () => {
+  return(
+    <>
+      <h1>{'404'}</h1>
+      <p>{'Whoops, we cound\'t find that!'}</p>
+      <a href='/'>Go Home</a>
+    </>
+  )
+}
+
+export default NotFound
