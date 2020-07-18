ffdf41759f3d890234aff5d160d737cddee3db0d
hi-matbub <6matbub@gmail.com>
2020-07-17 22:33:27 -0700

    skeleton 👻

--- src/comps/grid.jsx
+++ src/comps/grid.jsx
@@ -1,45 +1,25 @@
 import React from "react";
 import Box from "./box";
 
+const row = { display: "flex" };
+
 const Grid = () => {
   return (
     <div>
-      <div className="container-fluid">
-        <div className="row">
-          <div className="col">
-            <Box grid="1" />
-          </div>
-          <div className="col">
-            <Box grid="2" />
-          </div>
-          <div className="col">
-            <Box grid="3" />
-          </div>
-        </div>
-
-        <div className="row">
-          <div className="col">
-            <Box grid="4" />
-          </div>
-          <div className="col">
-            <Box grid="5" />
-          </div>
-          <div className="col">
-            <Box grid="6" />
-          </div>
-        </div>
-
-        <div className="row">
-          <div className="col">
-            <Box grid="7" />
-          </div>
-          <div className="col">
-            <Box grid="8" />
-          </div>
-          <div className="col">
-            <Box grid="9" />
-          </div>
-        </div>
+      <div style={row}>
+        <Box grid={1} />
+        <Box grid={2} />
+        <Box grid={3} />
+      </div>
+      <div style={row}>
+        <Box grid={1} />
+        <Box grid={2} />
+        <Box grid={3} />
+      </div>
+      <div style={row}>
+        <Box grid={1} />
+        <Box grid={2} />
+        <Box grid={3} />
       </div>
     </div>
   );
