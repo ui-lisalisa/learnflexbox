5ee5c3f1637daa7fa56f5db19cf71639034b2873
hi-matbub <6matbub@gmail.com>
2020-07-21 22:57:55 -0700

    snapshot & building data .3

--- src/app.test.js
+++ src/app.test.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import renderer from 'react-test-renderer';
 import App from './app.jsx';
 
-it('Renders without crashing', () => {
+it('renders smoke test without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+it('renders snapshot as expected', () => {
+  const tree = renderer
+  .create(<App/>)
+  .toJSON();
+  expect(tree).toMatchSnapshot(); 
+})
