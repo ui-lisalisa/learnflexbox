c5a3b5326e11d8f352c740955402839da90a674d
hi-matbub <6matbub@gmail.com>
2020-07-28 15:28:57 -0700

    removing unused code

--- src/firebase/actions_.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import {db} from './admin';
-
-export const postCount = () => {
-  const counts = 1;
-  db.collection('count')
-    .add(counts)
-    .then((res) => console.log(res));
-};
