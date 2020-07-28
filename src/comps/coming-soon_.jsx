a1f58155c2081ebff41cdb87c9f719996c6ced87
hi-matbub <6matbub@gmail.com>
2020-07-28 08:06:23 -0700

    adding pages to dir

--- src/comps/coming-soon_.jsx
+++ src/comps/coming-soon_.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-//prettier ignore
+//prettier-ignore
 const ComingSoon = () => {
   return (
     <div>
-      <h1>{"It's almost here! Be the first to know,"}</h1>
+      <h1>{'It\'s almost here! Be the first to know,'}</h1>
       {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
       <a href="">Click here to Subscribe for Updates</a>
     </div>
