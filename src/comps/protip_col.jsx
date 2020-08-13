ba41dc6bc1cab293e6a612ddc83c1f96487540b1
hi-matbub <6matbub@gmail.com>
2020-08-13 00:36:47 -0700

    simplifying layout

--- src/comps/protip_col.jsx
+++ src/comps/protip_col.jsx
@@ -1,59 +1,12 @@
 import React from 'react';
-import ProTip from './protip';
 import '../styles/protip_col.css';
-import light from '../lib/static/light.svg';
 
-const ProTipCol = (props) => {
+const ProTipCol = () => {
   return (
     <div className="pro-tips-col">
       <span className="pro-tips-title">
-        <img src={light} alt="light bulb" />
-        <h2>{'Pro Tips'}</h2>
+        <h2>{'Try the Flexbox Generator!'}</h2>
       </span>
-
-      <div className="pro-tips-scroll">
-        <ProTip
-          author={'hi-matbub'}
-          content={'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'}
-        />
-        <ProTip
-          author={'hi-matbub'}
-          content={'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'}
-        />
-        <ProTip
-          author={'hi-matbub'}
-          content={'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'}
-        />
-        <ProTip
-          author={'hi-matbub'}
-          content={'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'}
-        />
-        <ProTip
-          author={'hi-matbub'}
-          content={'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'}
-        />
-        <ProTip
-          author={'hi-matbub'}
-          content={'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'}
-        />
-        <ProTip
-          author={'hi-matbub'}
-          content={'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'}
-        />
-        <ProTip
-          author={'hi-matbub'}
-          content={'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'}
-        />
-        <ProTip
-          author={'hi-matbub'}
-          content={'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'}
-        />
-        <ProTip
-          author={'hi-matbub'}
-          content={'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'}
-        />
-      </div>
-      <div className="fadedScroller_fade"></div>
     </div>
   );
 };
